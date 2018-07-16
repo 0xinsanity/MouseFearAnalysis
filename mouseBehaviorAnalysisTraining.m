87b66b596d94928b4505291d96c8c36cec075535
Noah Hanover <noahsahar@gmail.com>
2018-07-16 13:27:18 -0400

    tracks center of mass now

--- mouseBehaviorAnalysisTraining.m
+++ mouseBehaviorAnalysisTraining.m
@@ -1,21 +1,21 @@
 function finalValue = mouseBehaviorAnalysisTraining(filename, show_work, frame_start, frame_end)
-    VideoSize = [210 300];
+    %VideoSize = [210 300];
     
     scale = 1/9.75; % centimeter/pixel
     frameRate = 3.75; % frame/second
 
     videoReader = vision.VideoFileReader(filename);
     videoPlayer = vision.VideoPlayer('Position',[200,200,500,400]);
-    foregroundDetector = vision.ForegroundDetector('NumTrainingFrames', 500, ...
-                    'InitialVariance', 0.05);
-    blobAnalyzer = vision.BlobAnalysis('AreaOutputPort', false, ...
-                    'MinimumBlobArea', 70);
+    %foregroundDetector = vision.ForegroundDetector('NumTrainingFrames', 500, ...
+    %                'InitialVariance', 0.05);
+    %blobAnalyzer = vision.BlobAnalysis('AreaOutputPort', false, ...
+    %                'MinimumBlobArea', 70);
                 
-    kalmanFilter = []; isTrackInitialized = false;
-    velocityLabel = 0; velocityTotal = [];
+    %kalmanFilter = []; isTrackInitialized = false;
+    velocityTotal = [];
     
     center = [105 150];
-    oldPoints = [];
+    %oldPoints = [];
     
     for i=1:frame_start
         test = step(videoReader);
@@ -23,8 +23,11 @@ function finalValue = mouseBehaviorAnalysisTraining(filename, show_work, frame_s
     
     figure
     dist_from_center = [105 150];
+    previous_centroid = []; initialized = 0;
     for i=frame_start:1:frame_end
-        detectedLocationPoint = [0 0];
+        %detectedLocationPoint = [0 0];
+        velocityLabel = 0;
+        
         accurateBg = imread('./MouseVideobehaviorAnalysis/trainingNOMOUSE.png');
         accurateBg = imcrop(accurateBg, [52 15 167 179]);
         accurateBg = rgb2gray(accurateBg);
@@ -68,63 +71,74 @@ function finalValue = mouseBehaviorAnalysisTraining(filename, show_work, frame_s
         bw_file = bwmorph(bw_file, 'thin');
         bw_file = bwareaopen(bw_file, 200);
         %bw_file = bwmorph(bw_file, 'thick');
+        s = regionprops(bw_file,'centroid');
+        centroids = cat(1, s.Centroid);
+        centroid = centroids(1,:);
         
-        foregroundMask = foregroundDetector(im2single(bw_file));
-        foregroundMask = bwareaopen(foregroundMask, 15);
-        %foregroundMask = imfill(foregroundMask, 'holes');
-        
-        detectedLocation = step(blobAnalyzer,foregroundMask); % [x,y]
-        
-        isObjectDetected = size(detectedLocation, 1) > 0;
-
-        if ~isTrackInitialized
-            if isObjectDetected
-                kalmanFilter = configureKalmanFilter('ConstantAcceleration',...
-                          detectedLocation(1,:), [1 1 1]*1e5, [1, 10, 10], 25);
-                isTrackInitialized = true;
-            end
-
-        else 
-            if isObjectDetected 
-                predict(kalmanFilter);
-                trackedLocation = correct(kalmanFilter, detectedLocation(1,:));
-                
-                points = trackedLocation;
-                if ~isempty(oldPoints)
-                    % Calculate velocity (pixels/frame)
-                    vel_pix = sqrt(sum((points-oldPoints).^2,2));
-                    velocityLabel = vel_pix * frameRate * scale; % pixels/frame * frame/seconds * meter/pixels
-                    velocityTotal = [velocityTotal velocityLabel];
-                end
-                oldPoints = points;
-            end
-            %circle = [trackedLocation 3];
+        if initialized
+            vel_pix = pdist2(centroid, previous_centroid, 'euclidean');
+            velocityLabel = vel_pix * frameRate * scale; % pixels/frame * frame/seconds * meter/pixels
+            velocityTotal = [velocityTotal velocityLabel];
+        else
+            initialized = 1;
         end
-
         
-        detectedLocationPoint = mean(detectedLocation);
-            
-        if isnan(detectedLocationPoint) ~= [1 1] & numel(detectedLocationPoint) == 2
-            dist_from_center = [dist_from_center; center-detectedLocationPoint];
-            
-            %foregroundMask = insertObjectAnnotation(foregroundMask, 'circle', ...
-            %    [detectedLocationPoint 3], cellstr([num2str(velocityLabel) ' cm/sec']), 'Color', 'green');
-        end
+        previous_centroid = centroid;
+%         foregroundMask = foregroundDetector(im2single(bw_file));
+%         foregroundMask = bwareaopen(foregroundMask, 15);
+%         %foregroundMask = imfill(foregroundMask, 'holes');
+%         
+%         detectedLocation = step(blobAnalyzer,foregroundMask); % [x,y]
+%         
+%         isObjectDetected = size(detectedLocation, 1) > 0;
+% 
+%         if ~isTrackInitialized
+%             if isObjectDetected
+%                 kalmanFilter = configureKalmanFilter('ConstantAcceleration',...
+%                           detectedLocation(1,:), [1 1 1]*1e5, [1, 10, 10], 25);
+%                 isTrackInitialized = true;
+%             end
+% 
+%         else 
+%             if isObjectDetected 
+%                 predict(kalmanFilter);
+%                 trackedLocation = correct(kalmanFilter, detectedLocation(1,:));
+%                 
+%                 points = trackedLocation;
+%                 if ~isempty(oldPoints)
+%                     % Calculate velocity (pixels/frame)
+%                     vel_pix = sqrt(sum((points-oldPoints).^2,2));
+%                     velocityLabel = vel_pix * frameRate * scale; % pixels/frame * frame/seconds * meter/pixels
+%                     velocityTotal = [velocityTotal velocityLabel];
+%                 end
+%                 oldPoints = points;
+%             end
+%             %circle = [trackedLocation 3];
+%         end
+% 
+%         
+%         detectedLocationPoint = mean(detectedLocation);
+%             
+%         if isnan(detectedLocationPoint) ~= [1 1] & numel(detectedLocationPoint) == 2
+%             dist_from_center = [dist_from_center; center-detectedLocationPoint];
+%             
+%             %foregroundMask = insertObjectAnnotation(foregroundMask, 'circle', ...
+%             %    [detectedLocationPoint 3], cellstr([num2str(velocityLabel) ' cm/sec']), 'Color', 'green');
+%         end
             
         % ONLY IF YOU WANT TO SEE IT WORKING IN REAL TIME  
         if show_work
-            foregroundMask = im2single(foregroundMask);
-            if ~isempty(detectedLocation)
+            %foregroundMask = im2single(foregroundMask);
+            bw_file = im2single(bw_file);
+            %if ~isempty(detectedLocation)
                 % TODO: Find a way to fill in mouse
                 %foregroundMask = regionfill(foregroundMask, [3 3 3 3], [4 4 4 4]);
-            end
+            %end
             
-            for i = 1:size(detectedLocation)
-                foregroundMask = insertObjectAnnotation(foregroundMask, 'circle', ...
-                [detectedLocation(i,:) 3], cellstr(['Point ' num2str(i)]), 'Color', 'green');
-            end
-            
-            imshowpair(im2single(foregroundMask), im2single(bw_file), 'montage');
+            bw_file = insertObjectAnnotation(bw_file, 'circle', ...
+            [centroid 3], cellstr([num2str(velocityLabel) ' cm/sec']), 'Color', 'green');
+
+            imshowpair(im2single(colorImage), im2single(bw_file), 'montage');
         end
     end
 
