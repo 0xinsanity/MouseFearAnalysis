8bedf7d84a41d478c34666d96ac5f17056ed829c
Noah Hanover <noahsahar@gmail.com>
2018-07-18 12:43:10 -0400

    uncommented runAllVideos

--- runAllVideos.m
+++ runAllVideos.m
@@ -9,50 +9,50 @@ function runAllVideos()
    analyze(training, 'training', 'Tone', 120, 150);
    % Footshock 145-150 sec
    analyze(training, 'training', 'Footshock', 145, 150);
-%    
-%    % Recall
-%    % Tone 120-150 sec
-%    analyze(recall, 'recall', 'Tone1', 120, 150);
-%    % Tone 240-270 sec
-%    analyze(recall, 'recall', 'Tone2', 240, 270);
-%    % Tone 330-360 sec
-%    analyze(recall, 'recall', 'Tone3', 330, 360);
-%    % Tone 420-450 sec
-%    analyze(recall, 'recall', 'Tone4', 420, 450);
-%    % Tone 570-600 sec
-%    analyze(recall, 'recall', 'Tone5', 570, 600);
-%    % Tone 630-660 sec
-%    analyze(recall, 'recall', 'Tone6', 630, 660);
-%    % Tone 720-750 sec
-%    analyze(recall, 'recall', 'Tone7', 720, 750);
-%    % Tone 870-900 sec
-%    analyze(recall, 'recall', 'Tone8', 870, 900);
-%    % Tone 960-990 sec
-%    analyze(recall, 'recall', 'Tone9', 960, 990);
-%    % Tone 1100-1130 sec
-%    analyze(recall, 'recall', 'Tone10', 1100, 1130);
-%    
-%    % Extinction
-%    % Tone 120-150 sec
-%    analyze(extinction, 'extinction', 'Tone1', 120, 150);
-%    % Tone 240-270 sec
-%    analyze(extinction, 'extinction', 'Tone2', 240, 270);
-%    % Tone 330-360 sec
-%    analyze(extinction, 'extinction', 'Tone3', 330, 360);
-%    % Tone 420-450 sec
-%    analyze(extinction, 'extinction', 'Tone4', 420, 450);
-%    % Tone 570-600 sec
-%    analyze(extinction, 'extinction', 'Tone5', 570, 600);
-%    % Tone 630-660 sec
-%    analyze(extinction, 'extinction', 'Tone6', 630, 660);
-%    % Tone 720-750 sec
-%    analyze(extinction, 'extinction', 'Tone7', 720, 750);
-%    % Tone 870-900 sec
-%    analyze(extinction, 'extinction', 'Tone8', 870, 900);
-%    % Tone 960-990 sec
-%    analyze(extinction, 'extinction', 'Tone9', 960, 990);
-%    % Tone 1100-1130 sec
-%    analyze(extinction, 'extinction', 'Tone10', 1100, 1130);
+   
+   % Recall
+   % Tone 120-150 sec
+   analyze(recall, 'recall', 'Tone1', 120, 150);
+   % Tone 240-270 sec
+   analyze(recall, 'recall', 'Tone2', 240, 270);
+   % Tone 330-360 sec
+   analyze(recall, 'recall', 'Tone3', 330, 360);
+   % Tone 420-450 sec
+   analyze(recall, 'recall', 'Tone4', 420, 450);
+   % Tone 570-600 sec
+   analyze(recall, 'recall', 'Tone5', 570, 600);
+   % Tone 630-660 sec
+   analyze(recall, 'recall', 'Tone6', 630, 660);
+   % Tone 720-750 sec
+   analyze(recall, 'recall', 'Tone7', 720, 750);
+   % Tone 870-900 sec
+   analyze(recall, 'recall', 'Tone8', 870, 900);
+   % Tone 960-990 sec
+   analyze(recall, 'recall', 'Tone9', 960, 990);
+   % Tone 1100-1130 sec
+   analyze(recall, 'recall', 'Tone10', 1100, 1130);
+   
+   % Extinction
+   % Tone 120-150 sec
+   analyze(extinction, 'extinction', 'Tone1', 120, 150);
+   % Tone 240-270 sec
+   analyze(extinction, 'extinction', 'Tone2', 240, 270);
+   % Tone 330-360 sec
+   analyze(extinction, 'extinction', 'Tone3', 330, 360);
+   % Tone 420-450 sec
+   analyze(extinction, 'extinction', 'Tone4', 420, 450);
+   % Tone 570-600 sec
+   analyze(extinction, 'extinction', 'Tone5', 570, 600);
+   % Tone 630-660 sec
+   analyze(extinction, 'extinction', 'Tone6', 630, 660);
+   % Tone 720-750 sec
+   analyze(extinction, 'extinction', 'Tone7', 720, 750);
+   % Tone 870-900 sec
+   analyze(extinction, 'extinction', 'Tone8', 870, 900);
+   % Tone 960-990 sec
+   analyze(extinction, 'extinction', 'Tone9', 960, 990);
+   % Tone 1100-1130 sec
+   analyze(extinction, 'extinction', 'Tone10', 1100, 1130);
    
 end
 
@@ -65,8 +65,7 @@ function analyze(database, name, type, frame_start, frame_end)
     %fprintf(fileID, [type ': \n']);
     printingMatrix = [type, "Velocity (cm/sec)", "Distance From Center (cm)", "Average Placement-X (cm)", "Average Placement-Y (cm)", "Mean Area (cm^2)", "Average Movement in Place (sec^2)", "Time On Wall (sec) - NOT DONE YET"];
     
-    %for i = 1:1:length(database)
-    for i = 1:1:2
+    for i = 1:1:length(database)
         full_name = [database(i).folder, '/', database(i).name];
         finalMetrics = mouseBehaviorAnalysis(full_name, 0, frame_start, frame_end);
         
